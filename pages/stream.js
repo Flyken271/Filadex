ae46268acf2df54c8b1249620fbc7a211cc3627b
Flyken271 <jaredcollins99@gmail.com>
2020-10-12 19:09:19 -0400

    Added streaming service

---
+++ pages/stream.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import VideoPlayer from "./components/VideoPlayer";
+
+export default function Home() {
+  const videoplayeroptions = {
+    autoplay: true,
+    controls: true,
+    sources: [
+      {
+        src: "http://98.237.41.63:8050/hls/flyken/index.m3u8",
+        type: "application/x-mpegurl",
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.player}>
+      <Head>
+        <title>Filadex - Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <VideoPlayer {...videoplayeroptions} />
+    </div>
+  );
+}
